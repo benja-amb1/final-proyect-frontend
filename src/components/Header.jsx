0db1e767b4e8ae2666dab950cc9023716537cc21
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-17 18:42:38 -0300

    add: react logo and first css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import ReactLogo from '../assets/images/react-icon.webp'
+
 const Header = () => {
   return (
     <header>
+
+      <div>
+        <Link to='/'>
+          <img src={ReactLogo} alt="Logo de React" />
+        </Link>
+      </div>
+
+
+
       <nav>
         <ul>
           <li>
