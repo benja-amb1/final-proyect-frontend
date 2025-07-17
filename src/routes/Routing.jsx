6eb9a42a05c6fca4e989a99af8ac8fb074b85edc
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-17 18:13:47 -0300

    add: routing base

---
+++ src/routes/Routing.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const Routing = () => {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route>
+
+        </Route>
+
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export { Routing }
