510fc7b5e5f86be9afc643430e043e705340da01
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-17 18:08:13 -0300

    add: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <nav>
-
+        <ul>
+          <li>
+            <Link>Inicio</Link>
+          </li>
+          <li>
+            <Link>Dashboard</Link>
+          </li>
+          <li>
+            <Link>Login</Link>
+          </li>
+          <li>
+            <Link>Registro</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   )
