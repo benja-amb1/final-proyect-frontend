bd7f746b5c7cd5aa4f198957d6a21b2daec9ead1
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-17 18:10:58 -0300

    add: layout y footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+
+  const date = new Date().getFullYear();
+
   return (
     <footer>
-      <p>Sitio desarrollado por Benja.</p>
+      <p>Sitio desarrollado por Benja. Todos los derechos reservados &copy; {date}.</p>
     </footer>
   )
 }
