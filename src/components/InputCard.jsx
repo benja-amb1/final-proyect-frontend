8f4012db7ef231fe9280ee52d41ee0582e88fe97
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-17 18:19:42 -0300

    add: input card

---
+++ src/components/InputCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputCard = ({ type, placeholder }) => {
+  return (
+    <>
+      <input type={type} placeholder={placeholder} />
+    </>
+  )
+}
+
+export default InputCard
