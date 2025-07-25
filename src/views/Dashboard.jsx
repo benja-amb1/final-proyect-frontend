14373f7b749edab3eeb54e287f63b449fc81d996
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-25 09:55:35 -0300

    add: POST para agregar productos y estilos

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from '../components/Layout'
 
 const Dashboard = () => {
+
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const [producto, setProducto] = useState(null);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const clearMsg = () => {
+    setTimeout(() => {
+      setError('');
+      setSuccess('');
+    }, 3000);
+  }
+
+  const agregarProducto = async (e) => {
+    e.preventDefault();
+
+    if (!image || !title || !price || isNaN(price) || price < 0) {
+      setError('Todos los campos son requeridos y válidos.')
+      clearMsg()
+      return;
+    }
+
+    try {
+      const res = await fetch('https://fakestoreapi.com/products',
+        { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ title, price, image }) }
+      );
+
+      const data = await res.json();
+
+      setProducto(data)
+      setSuccess('Producto agregado correctamente.')
+      clearMsg()
+
+      setTitle('');
+      setPrice('');
+      setImage('');
+    } catch (error) {
+      console.log(error);
+      setError('Error al agregar producto.')
+    }
+  }
+
   return (
     <Layout>
-      <form className='agregar-form'>
+      <form className='agregar-form' onSubmit={agregarProducto}>
         <h2>Agregar Producto</h2>
-        <label>Agregar nuevo producto:</label>
-        <input type="text" placeholder='Nombre del producto...' />
+        <label>Titulo:</label>
+        <input type="text" placeholder='Titulo' value={title} onChange={e => setTitle(e.target.value)} />
+        <label>Precio:</label>
+        <input type="number" min={0} placeholder='Precio' value={price} onChange={e => setPrice(e.target.value)} />
+        <label>Imagen:</label>
+        <input type="file" placeholder='Imagen' value={image} onChange={e => setImage(e.target.value)} />
         <button>Agregar</button>
+        {error && <p className='msgError'>{error}</p>}
+        {success && <p className='msgSuccess'>{success}</p>}
       </form>
 
     </Layout>
