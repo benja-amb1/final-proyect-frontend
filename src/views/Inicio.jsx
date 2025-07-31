58cb3a2693d9fa5d7c31604bec6eed955f04330d
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-31 20:02:37 -0300

    add: formas de entrega en inicio y css

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,15 +1,47 @@
 import React from 'react'
 import { Layout } from '../components/Layout'
 import ProductCard from '../components/ProductCard'
+import { Link } from 'react-router-dom'
+import CreditCard from '../assets/icons/credit-card.svg'
+import Shield from '../assets/icons/shield.svg'
+import Truck from '../assets/icons/truck.svg'
 
 const Inicio = () => {
   return (
     <Layout>
-      <div id='inicio'>
-        <h1>Bienvenido a Products Map!</h1>
-        <h2>Aquí encontrarás los mejores productos al mejor precio.</h2>
-      </div>
+      <section>
+        <div id='inicio'>
+          <h1>Bienvenido a Products Map!</h1>
+          <h2>Aquí encontrarás los mejores productos al mejor precio.</h2>
+        </div>
+      </section>
       <ProductCard />
+
+      <section className='entregas'>
+        <div className='entregas-cards'>
+
+          <div className='entrega-item'>
+            <img src={CreditCard} alt="Icono de Tarjeta de Crédito" />
+            <h3>Hasta 18 cuotas</h3>
+            <p>abonando con tarjetas de crédito</p>
+          </div>
+
+          <div className='entrega-item'>
+            <img src={Truck} alt="Icono de Camión de Envío" />
+            <h3>Envíos a todo el país</h3>
+            <p>a través de OCA</p>
+          </div>
+
+          <div className='entrega-item'>
+            <img src={Shield} alt="Icono de Garantía" />
+            <h3>Garantía oficial</h3>
+            <p>de hasta 36 meses en todos los productos</p>
+          </div>
+
+        </div>
+
+        <Link to='/contacto' className='btn-contactar'>Contactar</Link>
+      </section>
     </Layout>
   )
 }
